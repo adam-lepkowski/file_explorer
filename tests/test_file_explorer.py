f857c1ecc38fca7376bc94d0c440c493e68c28c9
adam <adam.lepkowski@hotmail.com>
2022-06-17 17:55:58 +0200

    Refactor get_content test

--- tests/test_file_explorer.py
+++ tests/test_file_explorer.py
@@ -9,17 +9,17 @@ class TestGetContent(unittest.TestCase):
     def setUp(self):
         self.fe = FileExplorer()
 
-    @patch("file_explorer.os.scandir")
-    def test_get_content(self, scandir_mock):
+    @patch("file_explorer.pathlib.Path.iterdir")
+    def test_get_content(self, iterdir_mock):
         mocks = [Mock() for _ in range(4)]
         for i, mock in enumerate(mocks):
             mock.is_dir.return_value = True if i % 2 == 0 else False
             mock.is_file.return_value = False if i % 2 == 0 else True
-            mock.path = "dir_path" if i % 2 == 0 else "file_path"
-        scandir_mock.return_value = mocks
-        result = self.fe.get_content("path_not_relevant_to_test")
+            mock.__str__ = "dir_path" if i % 2 == 0 else "file_path"
+        iterdir_mock.return_value = mocks
+        result = self.fe.get_content(".")
         expected = {
-            "files": ["file_path", "file_path"],
-            "dirs": ["dir_path", "dir_path"]
+            "files": [mocks[1], mocks[3]],
+            "dirs": [mocks[0], mocks[2]]
         }
         self.assertEqual(expected, result)
