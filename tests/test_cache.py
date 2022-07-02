b0f16704b55f54fb8eb762a8410f84b694d507a3
adam <adam.lepkowski@hotmail.com>
2022-07-02 10:48:50 +0200

    Test undo and redo

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,5 +1,7 @@
 import unittest
 
+from parameterized import parameterized
+
 from cache import Cache
 
 
@@ -18,3 +20,29 @@ class TestStore(unittest.TestCase):
         self.cache.store("barfoo")
         self.assertEqual(0, self.cache.current)
         self.assertEqual(["barfoo"], self.cache.items)
+
+
+class TestUndoRedo(unittest.TestCase):
+
+    def setUp(self):
+        self.cache = Cache()
+
+    @parameterized.expand([
+        ("undo", 1, 0),
+        ("nothing_to_undo", -1, -1),
+        ("only_one_action", 0, 0)
+    ])
+    def test_undo(self, name, current, expected):
+        self.cache.current = current
+        self.cache.undo()
+        self.assertEqual(self.cache.current, expected)
+
+    @parameterized.expand([
+        ("redo", 0, ["foo", "bar"], 1),
+        ("nothing_to_redo", 1, ["foo", "bar"], 1)
+    ])
+    def test_redo(self, name, current, items, expected):
+        self.cache.current = current
+        self.cache.items = items
+        self.cache.redo()
+        self.assertEqual(self.cache.current, expected)
