3e7292af81df636d67fdc48783cebfcd0486a40f
adam <adam.lepkowski@hotmail.com>
2022-07-20 17:06:55 +0200

    Test get_content

--- tests/test_facade.py
+++ tests/test_facade.py
@@ -1,5 +1,8 @@
 import unittest
+from unittest.mock import patch
 from pathlib import Path
+
+from parameterized import parameterized
 
 from explorer import Facade
 
@@ -18,3 +21,18 @@ class TestGetContent(unittest.TestCase):
 
     def setUp(self):
         self.facade = Facade()
+        self.content = {
+            "dirs": [Path("dir1")],
+            "files": [Path("file1.py")]
+        }
+
+    @patch("explorer.facade.os.path.getmtime", return_value=1)
+    @patch("explorer.facade.FileExplorer.get_content")
+    def test_get_content(self, mock_explorer, mock_getmtime):
+        mock_explorer.return_value = self.content
+        expected = [
+            ("dir1", "1970/01/01 01:00:01", "dirs"),
+            ("file1.py", "1970/01/01 01:00:01", "files")
+        ]
+        result = self.facade.get_content("foo/bar")
+        self.assertEqual(expected, result)
