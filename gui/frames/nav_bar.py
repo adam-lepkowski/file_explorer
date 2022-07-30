4b9a176670a41abea533cce310fe8067407a47fa
adam <adam.lepkowski@hotmail.com>
2022-07-30 08:14:25 +0200

    Add addr_var

--- gui/frames/nav_bar.py
+++ gui/frames/nav_bar.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from tkinter import ttk
 
 
@@ -18,6 +19,8 @@ class NavBar(ttk.Frame):
         confirm path entered to addr_bar
     addr_bar : ttk.Entry
         hold currently viewed directory absolute path
+    addr_var : tk.StringVar
+        address bar string variable
     """
 
     def __init__(self, root):
@@ -25,8 +28,9 @@ class NavBar(ttk.Frame):
         self["padding"] = 5
         self.parent_btn = ttk.Button(self, style="ExpBar.TButton", text="^")
         self.parent_btn.grid(row=0, column=0, sticky="we")
+        self.addr_var = tk.StringVar(self)
         # add validatecommand option to entry initialization - validate on focusout
-        self.addr_bar = ttk.Entry(self)
+        self.addr_bar = ttk.Entry(self, textvariable=self.addr_var)
         self.addr_bar.grid(row=0, column=1, sticky="we")
         self.cnf_addr_btn = ttk.Button(self, style="ExpBar.TButton", text=">")
         self.cnf_addr_btn.grid(row=0, column=2, sticky="we")
